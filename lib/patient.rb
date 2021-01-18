8b3f89bdca3eb5e93ebe4e00a53352bec131db37
Harrf Akbar <harrf_akbar@yahoo.com>
2021-01-17 20:02:33 -0800

    commit tree 4be6dca697787ca0973de7f046dc4cbdbe14fdfa

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,1 +1,34 @@
+require 'pry'
+class Patient
 
+    attr_accessor :name 
+
+    @@all = []
+
+    def self.all
+        @@all 
+    end
+
+    def initialize(name)
+        @name = name 
+        @@all << self 
+    end
+    
+    def appointments 
+        Appointment.all.select do |appointment|
+            appointment.patient == self
+        end
+    end
+
+    def new_appointment(date, doctor)
+        Appointment.new(date, self, doctor)
+    end
+
+    def doctors 
+        appointments.map do |appointment|
+            appointment.doctor             
+        end
+    end
+
+
+end
